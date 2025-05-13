a8b63edf947ced8976e1d207209e9dc9ff9b74ec
Diego Alonso Alvarez <d.alonso-alvarez@imperial.ac.uk>
2025-05-13 17:49:32 +0100

    :sparkles: Add more fields to admin list.

--- main/admin.py
+++ main/admin.py
@@ -8,4 +8,18 @@ from .models import ActivityCode, Department, Project, User
 admin.site.register(User, UserAdmin)
 admin.site.register(Department)
 admin.site.register(ActivityCode)
-admin.site.register(Project)
+
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    """Admin class for the Project model."""
+
+    list_display = (
+        "name",
+        "nature",
+        "department",
+        "status",
+        "charging",
+        "start_date",
+        "end_date",
+    )
