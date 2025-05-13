18fb9312fcd6ad223e65faf035b429db60ec17fd
Diego Alonso Alvarez <d.alonso-alvarez@imperial.ac.uk>
2025-05-13 17:35:57 +0100

    :sparkles: Add Project to Admin.

--- main/admin.py
+++ main/admin.py
@@ -3,8 +3,9 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
-from .models import ActivityCode, Department, User
+from .models import ActivityCode, Department, Project, User
 
 admin.site.register(User, UserAdmin)
 admin.site.register(Department)
 admin.site.register(ActivityCode)
+admin.site.register(Project)
