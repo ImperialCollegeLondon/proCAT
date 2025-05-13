e71622a422f35b85f452bc6bce54342e69fcc8c4
Diego Alonso Alvarez <d.alonso-alvarez@imperial.ac.uk>
2025-05-13 15:03:04 +0100

    :sparkles: Add Department model.

--- main/models.py
+++ main/models.py
@@ -1,10 +1,43 @@
 """Models module for main app."""
 
 from django.contrib.auth.models import AbstractUser
-from django.db import models  # noqa: F401
+from django.db import models
 
 
 class User(AbstractUser):
     """Custom user model."""
 
     pass
+
+
+class Department(models.Model):
+    """Model to manage the departments.
+
+    You can find the faculties and potential departments in:
+
+    https://www.imperial.ac.uk/faculties-and-departments/
+    """
+
+    _FACULTY = (
+        ("engineering", "Faculty of Engineering"),
+        ("medicine", "Faculty of Medicine"),
+        ("fons", "Faculty of Natural Sciences"),
+        ("ibs", "Imperial Business School"),
+        ("other", "Other"),
+    )
+
+    name = models.CharField(
+        "Department",
+        unique=True,
+        blank=False,
+        null=False,
+        help_text="Name of the department, centre or school.",
+    )
+
+    faculty = models.CharField(
+        "Faculty",
+        blank=False,
+        null=False,
+        choices=_FACULTY,
+        help_text="Faculty the department belongs to.",
+    )
