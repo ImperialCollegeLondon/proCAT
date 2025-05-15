68e53c789d4d2c4871c48f1833c31cda6ec6926d
Diego Alonso Alvarez <d.alonso-alvarez@imperial.ac.uk>
2025-05-15 09:20:57 +0100

    :bug: Properly configure login url.

--- procat/settings/settings.py
+++ procat/settings/settings.py
@@ -118,7 +118,13 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 # Custom settings
-INSTALLED_APPS += ["main", "django_bootstrap5", "crispy_forms", "crispy_bootstrap5"]
+INSTALLED_APPS += [
+    "main",
+    "django_bootstrap5",
+    "crispy_forms",
+    "crispy_bootstrap5",
+    "django_tables2",
+]
 
 MIDDLEWARE.insert(1, "whitenoise.middleware.WhiteNoiseMiddleware")
 STATIC_ROOT = BASE_DIR / "staticfiles"
@@ -130,6 +136,6 @@ CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 DJANGO_TABLES2_TEMPLATE = "django_tables2/bootstrap5.html"
 
-LOGIN_URL = "/login/"
+LOGIN_URL = "/auth/login/"
 LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "/"
