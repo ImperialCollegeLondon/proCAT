fcf0986f28467e44723d962d57a848e1e3431c08
Diego Alonso Alvarez <d.alonso-alvarez@imperial.ac.uk>
2025-05-15 06:12:29 +0100

    :sparkles: Add registration page.

--- main/urls.py
+++ main/urls.py
@@ -6,4 +6,7 @@ from . import views
 
 app_name = "main"
 
-urlpatterns = [path("", views.index, name="index")]
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("register/", views.RegistrationView.as_view(), name="auth_register"),
+]
