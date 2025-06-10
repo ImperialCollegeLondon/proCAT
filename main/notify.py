55c6af14f0005299ac4db8aabeef4886250d3de9
Saranjeet Kaur <s.bhogal@imperial.ac.uk>
2025-06-10 21:28:03 +0100

    :bug: include Project

--- main/notify.py
+++ main/notify.py
@@ -2,6 +2,8 @@
 
 from django.core.mail import send_mail
 
+from .models import Project
+
 lead_message = """The project {project_name} is due for completion soon.
 
 Please check the project status and make sure to update your time spent
@@ -9,15 +11,16 @@ on the project.
 """
 
 
-def notify_lead(project: object) -> None:
+def notify_lead(project: Project) -> None:
     """Send notification to the project lead about the project status."""
     subject = f"Project {project.name} is due for completion soon!"
     message = lead_message.format(project_name=project.name)
 
-    send_mail(
-        subject,
-        message,
-        from_email=None,
-        recipient_list=[project.lead.email],
-        fail_silently=False,
-    )
+    if project.lead is not None and project.lead.email:
+        send_mail(
+            subject,
+            message,
+            from_email=None,
+            recipient_list=[project.lead.email],
+            fail_silently=False,
+        )
