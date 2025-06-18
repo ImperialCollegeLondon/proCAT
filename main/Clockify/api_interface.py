dd12974398acac6041e751ed8e786a61390f9642
Sahil Raja <s.raja@imperial.ac.uk>
2025-06-18 11:59:27 +0100

    mypy error fix

--- main/Clockify/api_interface.py
+++ main/Clockify/api_interface.py
@@ -3,6 +3,7 @@
 import json
 import os
 
+import dict
 import requests
 
 WORKSPACE_ID = os.getenv("CLOCKIFY_WORKSPACE_ID")
@@ -18,7 +19,7 @@ class ClockifyAPI:
         self.reports_base_url = "https://reports.api.clockify.me/v1"
         self.headers = {"Content-Type": "application/json", "X-Api-Key": self.api_key}
 
-    def get_time_entries(self, payload: dict) -> dict[str, object]:
+    def get_time_entries(self, payload: dict) -> dict:
         """Retrieve detailed time entries for a specified workspace using the API."""
         url = f"{self.reports_base_url}/workspaces/{WORKSPACE_ID}/reports/detailed"
 
