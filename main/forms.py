38617533fc2245d6d898e03d6f1b3005fd9e40b2
Max Gamill <maxgamill@live.com>
2025-11-20 17:03:06 +0000

    streamline form creation relying solely on CreateView

--- main/forms.py
+++ main/forms.py
@@ -5,8 +5,6 @@ from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
 from django.utils import timezone
 
-from .models import Department
-
 
 class CustomUserCreationForm(UserCreationForm):  # type: ignore [type-arg]
     """Form to create users with the custom User model.
@@ -54,104 +52,4 @@ class CostRecoveryForm(forms.Form):
         label="Year",
         coerce=int,
         help_text="Year for which to generate the charges report.",
-    )
-
-
-class CreateProjectForm(forms.Form):
-    """Form to use in the Cost Recovery view."""
-
-    _NATURE = ((0, "Support"), (1, "Standard"))
-    _STATUS = (
-        (0, "Tentative"),
-        ("Confirmed", "Confirmed"),
-        ("Active", "Active"),
-        ("Finished", "Finished"),
-        ("Not done", "Not done"),
-    )
-    _CHARGING = (
-        ("Actual", "Actual"),
-        ("Pro-rata", "Pro-rata"),
-        ("Manual", "Manual"),
-    )
-
-    name = forms.CharField(
-        label="Project Name",
-        required=True,
-        help_text="Name of the project.",
-    )
-    nature = forms.TypedChoiceField(
-        label="Nature",
-        choices=_NATURE,
-        required=True,
-        coerce=int,
-        help_text="Nature of the project.  Typically, support projects cannot be "
-        "allocated to sprints easily, the work there is more lightweight and ad hoc, "
-        "sometimes at short notice.",
-    )
-    pi = forms.CharField(
-        label="Principal Investigator",
-        required=True,
-        help_text="Name of the principal investigator responsible for the project. "
-        "It should be the actual grant holder, not the main point of contact",
-    )
-
-    department = forms.ModelChoiceField(
-        label="Department",
-        queryset=Department.objects.values_list("name"),
-        required=True,
-        help_text="The department in which the research project is based, primarily.",
-    )
-    start_date = forms.DateField(
-        label="Start date",
-        required=False,
-        help_text="Start date for the project.",
-        widget=forms.widgets.SelectDateWidget(attrs=dict(type="date")),
-    )
-    end_date = forms.DateField(
-        label="End date",
-        required=False,
-        help_text="End date for the project.",
-    )
-    lead = forms.ModelChoiceField(
-        label="RSE Lead",
-        required=False,
-        queryset=get_user_model().objects.values_list("username"),
-        help_text="Project lead from the RSE side.",
-    )
-    status = forms.TypedChoiceField(
-        label="Status",
-        required=True,
-        initial="Tentative",
-        choices=_STATUS,
-        help_text="Status of the project. Unless the status is 'Tentative' or "
-        "'Not done', most other fields are mandatory.",
-    )
-    charging = forms.TypedChoiceField(
-        label="Charging method",
-        required=True,
-        initial="Actual",
-        choices=_CHARGING,
-        help_text="Method for charging the costs of the project. 'Actual' is based"
-        " on timesheet records. 'Pro-rata' charges the same amount every month. "
-        "Finally, in 'Manual' the charges are scheduled manually.",
-    )
-    # Want to remove?
-    # notifications_effort = models.JSONField(
-    #     "Summary of effort left notifications",
-    #     default=dict,
-    #     blank=True,
-    #     help_text="Summarises the notifications sent when an effort threshold "
-    #     "is crossed and the corresponding dates.",
-    # )
-    # notifications_weeks = models.JSONField(
-    #     "Summary of weeks left notifications",
-    #     default=dict,
-    #     blank=True,
-    #     help_text="Summarises the notifications sent when the weeks threshold "
-    #     "is crossed and the corresponding dates.",
-    # )
-    clockify_id = forms.CharField(
-        label="Clockify ID",
-        required=False,
-        help_text="The ID of the project in Clockify, if applicable.",
     )
