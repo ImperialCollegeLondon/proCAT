47e1068b91879e94bb5376160ede95a1d7954651
Max-Gamill <maxgamill@live.com>
2025-12-16 13:14:27 +0000

    Add phase lifetime span warning and tests

--- tests/main/test_models_utils.py
+++ tests/main/test_models_utils.py
@@ -1,5 +1,9 @@
 """Tests for the model utils."""
 
+from datetime import timedelta
+
+from django.utils import timezone
+
 
 class TestProjectWarnings:
     """Tests for the different combinations of warnings."""
@@ -10,6 +14,14 @@ class TestProjectWarnings:
 
         project = models.Project.objects.get(name="ProCAT")
 
+        # Patch project with spanning phase
+        models.ProjectPhase.objects.create(
+            project=project,
+            value=1,
+            start_date=timezone.now().date(),
+            end_date=timezone.now().date() + timedelta(days=42),
+        )
+
         assert project.warnings == []
         assert not project.has_warnings
 
@@ -19,5 +31,22 @@ class TestProjectWarnings:
 
         project = models.Project.objects.get(name="ProCAT")
 
+        # Patch project with spanning phase
+        models.ProjectPhase.objects.create(
+            project=project,
+            value=1,
+            start_date=timezone.now().date(),
+            end_date=timezone.now().date() + timedelta(days=42),
+        )
+
         assert project.warnings == ["No funding defined for the project."]
         assert project.has_warnings
+
+    def test_warn_phase_lifetime(self, project_static, phase):
+        """Tests Warning model mixin for generated warnings."""
+        from main import models
+
+        project_static = models.Project.objects.get(name="ProCATv2")
+
+        assert project_static.warnings == ["Phases do not span project lifetime."]
+        assert project_static.has_warnings
